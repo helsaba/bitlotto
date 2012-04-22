7ed5d49d3bfe1335960b61f429faf4d8f9c9aa77
Stephen Gornick <sgornick@digicoast.com>
2012-04-21 23:15:13 -0700

    Update to latest

--- bitlotto.com/index.php
+++ bitlotto.com/index.php
@@ -9,6 +9,21 @@
 
   <title>BitLotto - the Bitcoin lottery</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31046208-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>
 
 <body>
@@ -20,7 +35,7 @@
 <td align="right"><span style="font-size: x-large;">
 May 2 Jackpot so far:
 <font color="#FF0000">
-199.25 BTC</font></span></td>
+226.47 BTC</font></span></td>
 </tr>
 </table>  
        
